598fc7857a5b3c6f2a9d7a778a567df04cd04525
Vasilijs SPECTRE <v1ac95@gmail.com>
2019-07-01 19:58:29 +0300

    In process of making recyclerView

---
+++ app/src/main/java/com/millenial/stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,21 @@
+package com.millenial.stormy.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.millenial.stormy.R;
+
+public class HourlyForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+
+
+    }
+
+}
